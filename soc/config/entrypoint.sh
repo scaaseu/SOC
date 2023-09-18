ba59d8729b6753e485c4135b2ed1bd3798557526
Adrian Eisenmeier <adrian@scaas.eu>
2023-09-18 10:33:52 +0000

    fix

--- soc/config/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-echo set tor configuration
-echo 'HiddenServiceDir /var/lib/tor/hidden_service/' >> /etc/tor/torrc 
-
-echo tor configuration file
-cat /etc/tor/torrc
-
-tail -f /dev/null 
