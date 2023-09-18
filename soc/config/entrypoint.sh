6dc5501edaa892ea43ea3e5aad579f4059377e3d
Adrian Eisenmeier <adrian@scaas.eu>
2023-09-18 10:21:17 +0000

    tmp

--- soc/config/entrypoint.sh
+++ soc/config/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/zsh
 
-myIp="$(hostname -I | cut -f1 -d' ')"
-echo "public ip: "  $myIp
-
 echo set tor configuration
-sed '/^\[global\]/a\HiddenServiceDir /var/lib/tor/hidden_service/' /etc/tor/torrc  
+echo 'HiddenServiceDir /var/lib/tor/hidden_service/' >> /etc/tor/torrc 
 
 echo tor configuration file
 cat /etc/tor/torrc
+
+nohup /bin/zsh & 
